28e39866a07d8321ce724896a3a1c00e831d6f4f
Aarif <coolkidar@gmail.com>
2020-11-22 00:10:02 -0500

    Testing text clickables along with imageview

--- app/src/main/java/com/leerazak/ticket/MainActivity.java
+++ app/src/main/java/com/leerazak/ticket/MainActivity.java
@@ -9,19 +9,31 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView box1;
     //TESTING
-    EditText e1, e2;
-    TextView t1, ticket_text;
-    int num1, num2;
+
     Button main_ticket_button;
+    //this extra_message allows communication between both activities
     public static final String EXTRA_MESSAGE = "com.example.leerazak.extra.MESSAGE";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //important stuff ^^
 
+        //Based on the first museum selection
+        box1 = (TextView) findViewById(R.id.main_loc_1);
+        box1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationOne();
+            }
+        });
+        //Testing communication
         main_ticket_button = (Button) findViewById(R.id.main_ticket_button);
         main_ticket_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+    public void locationOne(){
+        Intent intent = new Intent(this, TicketActivity.class);
+        String message = "LOCATION1 TEST";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
     /*public void launchTicketActivity(View view){
         Intent intent = new Intent(this, TicketActivity.class);
         String message = "hello from main!";
@@ -49,53 +69,5 @@ public class MainActivity extends AppCompatActivity {
     }*/
 
 
-    public boolean getNumbers()
-    {
 
-        // defining the edit text 1 to e1
-        e1 = (EditText)findViewById(R.id.main_num1);
-
-        // defining the edit text 2 to e2
-        e2 = (EditText)findViewById(R.id.main_num2);
-
-        // defining the text view to t1
-        t1 = (TextView)findViewById(R.id.main_output);
-
-        // taking input from text box 1
-        String s1 = e1.getText().toString();
-
-        // taking input from text box 2
-        String s2 = e2.getText().toString();
-
-        // condition to check if box is not empty
-        if ((s1.equals(null) && s2.equals(null))
-                || (s1.equals("") && s2.equals(""))) {
-
-            String result = "Please enter a value";
-            t1.setText(result);
-
-            return false;
-        }
-        else {
-            // converting string to int.
-            num1 = Integer.parseInt(e1.getText().toString());
-
-            // converting string to int.
-            num2 = Integer.parseInt(e2.getText().toString());
-        }
-
-        return true;
-    }
-
-
-    public void doSum(View v)
-    {
-
-        // get the input numbers
-
-        if (getNumbers()) {
-            int sum = num1 + num2;
-            t1.setText(Integer.toString(sum));
-        }
-    }
 }
--- app/src/main/java/com/leerazak/ticket/TicketActivity.java
+++ app/src/main/java/com/leerazak/ticket/TicketActivity.java
@@ -5,20 +5,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class TicketActivity extends AppCompatActivity{
     TextView ticket_text;
-
+    EditText e1, e2;
+    TextView t1;
+    ImageView photo;
+    int num1, num2;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_ticket);
+
+            //Generate an intent, and check from where we got the input from
             Intent intent = getIntent();
             String temp = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-            TextView textView = findViewById(R.id.ticket_text);
-            textView.setText(temp);
+            if(temp.equalsIgnoreCase("hello from main!")){
+                //testing where we got the button from
+                TextView textView = findViewById(R.id.ticket_text);
+                textView.setText(temp);
+            }else if(temp.equals("LOCATION1 TEST")){
+                TextView textView = findViewById(R.id.ticket_text);
+                textView.setText(temp);
+                ImageView picture = findViewById(R.id.ticket_picture);
+                picture.setImageResource(R.drawable.pogchamp);
+
+            }
+
+
         }
+
+        //dummy stuff to test input and output stuff
+    public boolean getNumbers()
+    {
+
+        // defining the edit text 1 to e1
+        e1 = (EditText)findViewById(R.id.main_num1);
+
+        // defining the edit text 2 to e2
+        e2 = (EditText)findViewById(R.id.main_num2);
+
+        // defining the text view to t1
+        t1 = (TextView)findViewById(R.id.main_output);
+
+        // taking input from text box 1
+        String s1 = e1.getText().toString();
+
+        // taking input from text box 2
+        String s2 = e2.getText().toString();
+
+        // condition to check if box is not empty
+        if ((s1.equals(null) && s2.equals(null))
+                || (s1.equals("") && s2.equals(""))) {
+
+            String result = "Please enter a value";
+            t1.setText(result);
+
+            return false;
+        }
+        else {
+            // converting string to int.
+            num1 = Integer.parseInt(e1.getText().toString());
+
+            // converting string to int.
+            num2 = Integer.parseInt(e2.getText().toString());
+        }
+
+        return true;
+    }
+
+
+    public void doSum(View v)
+    {
+
+        // get the input numbers
+
+        if (getNumbers()) {
+            int sum = num1 + num2;
+            t1.setText(Integer.toString(sum));
+        }
+    }
 }
